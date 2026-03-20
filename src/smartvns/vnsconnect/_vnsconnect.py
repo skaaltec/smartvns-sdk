426984b6c359bd91aa94dd14c107938815370446
mehrler <mehrler@ethz.ch>
2026-03-20 12:36:41 +0100

    Only changes regarding the format

--- src/smartvns/vnsconnect/_vnsconnect.py
+++ src/smartvns/vnsconnect/_vnsconnect.py
@@ -367,17 +367,18 @@ class Stimulator(Tracker):
 
         Args:
             cfg (StimConfig): The stimulation configuration to set on the device.
-                StimConfig:
-                    trigger_ms (int): The duration of the stimulation in milliseconds.
-                    forward_us (int): The duration of the forward phase of the stimulation pulse in microseconds.
-                    deadband_us (int): The duration of the deadband phase of the stimulation pulse in microseconds (the period after the forward phase during which no stimulation occurs).
-                    period_us (int): The total period of the stimulation pulse in microseconds (the time from the start of one pulse to the start of the next pulse).
-                    intensity_uA (int): The intensity of the stimulation in microamperes.
-                    retain_cfg (bool): Whether to retain the current configuration on the device after a power cycle (True) or not (False) (default: False).
+
             timeout (float): Timeout for the operation in seconds.
 
         Returns:
             None
+        StimConfig:
+            trigger_ms (int): The duration of the stimulation in milliseconds.
+            forward_us (int): The duration of the forward phase of the stimulation pulse in microseconds.
+            deadband_us (int): The duration of the deadband phase of the stimulation pulse in microseconds (the period after the forward phase during which no stimulation occurs).
+            period_us (int): The total period of the stimulation pulse in microseconds (the time from the start of one pulse to the start of the next pulse).
+            intensity_uA (int): The intensity of the stimulation in microamperes.
+            retain_cfg (bool): Whether to retain the current configuration on the device after a power cycle (True) or not (False) (default: False).
         """
         return self.run(
             write_stim_config_async(self._client, cfg),
